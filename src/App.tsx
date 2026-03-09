c29eecf0632364d291516f2ce4b7de3152af1217
STS-Engineer <administration.sts@avocarbon.com>
2026-03-09 13:13:49 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,30 @@
 import React from 'react';
 import Navbar from './components/Navbar.tsx';
 import RFQTable from './components/RFQTable.tsx';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
+
       <main className="main-content">
         <RFQTable />
       </main>
+
+      {/* Toast container for success / error messages */}
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+      />
+      
     </div>
   );
 }
